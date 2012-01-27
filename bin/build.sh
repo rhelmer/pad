9362233efb4c3a7183f5dfaa2e26ad3380504c4c
Jake Maul <nmaul@mozilla.com>
2012-01-26 19:03:07 -0700

    making bin/build.sh work properly on a RHEL with /etc/sysconfig/etherpad in place.

--- bin/build.sh
+++ bin/build.sh
@@ -22,7 +22,8 @@
 ################################################################################
 
 ETHERPADDIR="$(cd "$(dirname "$0")/.."; pwd)"
-source "$ETHERPADDIR/bin/exports.sh"
+#source "$ETHERPADDIR/bin/exports.sh"
+source "/etc/sysconfig/etherpad"
 
 # Rebuild jar
 ( cd "$ETHERPADDIR"/infrastructure; ./bin/makejar.sh; )
